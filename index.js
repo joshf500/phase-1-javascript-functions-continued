f8446fb9bd0e0b1e6dcc3f7353105e74db2517ea
Josh Feldman <joshfeldman500@gmail.com>
2023-03-20 16:46:33 -0400

    functions continued lab done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+activity=''
+function saturdayFun(activity){
+    if (activity==undefined){
+    return "This Saturday, I want to roller-skate!"
+    }
+    else{
+    return "This Saturday, I want to "+activity+"!"
+    }
+}
+const fn = mondayWork()
+function mondayWork(activity) {
+    if (activity==undefined){
+        return "This Monday, I will go to the office."
+        }
+         else{
+        return "This Monday, I will "+activity+"."
+        }
+}
+fn();
+
+
+function wrapAdjective(flair){
+    return function(part1){
+        if (part1==undefined){
+            part1=='special'
+        }
+        if (flair==undefined){
+            flair=='*'
+        }
+        return "You are "+flair+part1+flair+"!"
+        }
+    }
+
